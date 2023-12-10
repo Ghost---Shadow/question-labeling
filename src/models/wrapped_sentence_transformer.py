5115b9a6df2c08ec812c9e9cdafb274e5f9463ac
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2023-12-10 17:39:58 -0600

    Wrapped sentence transformer overfit test

--- src/models/wrapped_sentence_transformer.py
+++ src/models/wrapped_sentence_transformer.py
@@ -1,4 +1,5 @@
 from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import batch_to_device
 import torch
 
 
@@ -10,11 +11,19 @@ class WrappedSentenceTransformerModel:
         self.model = SentenceTransformer(checkpoint).to(device)
 
     def get_inner_products(self, query, documents):
-        all_sentences = [query, *documents]
-        all_embeddings = self.model.encode(all_sentences)
+        all_sentences = [query] + documents
+        # Convert sentences to input format expected by the model (e.g., tokenization)
+        # This depends on how SentenceTransformer expects its inputs
+        features = self.model.tokenize(all_sentences)
+        features = batch_to_device(features, self.device)
 
-        query_embedding = torch.tensor(all_embeddings[0], device=self.device)
-        query_embedding = query_embedding.unsqueeze(0)
-        document_embeddings = torch.tensor(all_embeddings[1:], device=self.device)
+        # Use model.forward() to maintain the computation graph
+        out_features = self.model.forward(features)
+        all_embeddings = out_features["sentence_embedding"]
 
-        return (query_embedding @ document_embeddings.T).squeeze()
+        # Extract query and document embeddings
+        query_embedding = all_embeddings[0].unsqueeze(0)
+        document_embeddings = all_embeddings[1:]
+
+        # Compute inner products
+        return torch.matmul(query_embedding, document_embeddings.T).squeeze()
--- src/models/wrapped_sentence_transformer_test.py
+++ src/models/wrapped_sentence_transformer_test.py
@@ -2,10 +2,13 @@ import unittest
 from models.wrapped_sentence_transformer import WrappedSentenceTransformerModel
 import numpy as np
 import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.cuda.amp import autocast, GradScaler
 
 
 # python -m unittest models.wrapped_sentence_transformer_test.TestWrappedSentenceTransformerModel -v
-# @unittest.skip("needs GPU")
+@unittest.skip("needs GPU")
 class TestWrappedSentenceTransformerModel(unittest.TestCase):
     # python -m unittest models.wrapped_sentence_transformer_test.TestWrappedSentenceTransformerModel.test_get_inner_products -v
     def test_get_inner_products(self):
@@ -38,3 +41,86 @@ class TestWrappedSentenceTransformerModel(unittest.TestCase):
         ]
 
         assert actual == expected, actual
+
+
+# python -m unittest models.wrapped_sentence_transformer_test.TestOverfit -v
+@unittest.skip("needs GPU")
+class TestOverfit(unittest.TestCase):
+    # python -m unittest models.wrapped_sentence_transformer_test.TestOverfit.test_overfit -v
+    def test_overfit(self):
+        config = {
+            "architecture": {
+                "semantic_search_model": {
+                    "checkpoint": "all-mpnet-base-v2",
+                    "device": "cuda:0",
+                }
+            }
+        }
+        wrapped_model = WrappedSentenceTransformerModel(config)
+        query = "What color is the fruit that Alice loves?"
+        documents = [
+            "What fruit does Alice love?",
+            "What fruit does Bob love?",
+            "What is the color of apple?",
+            "How heavy is an apple?",
+        ]
+        wrapped_model.model.train()
+        target = torch.tensor([[1.0, 0.0, 1.0, 0.0]], device="cuda:0")
+
+        optimizer = optim.AdamW(wrapped_model.model.parameters(), lr=1e-6)
+        loss_fn = nn.MSELoss()
+
+        # loss goes down
+        for _ in range(100):
+            # zero grad
+            optimizer.zero_grad()
+
+            inner_product = wrapped_model.get_inner_products(query, documents)
+
+            loss = loss_fn(inner_product.unsqueeze(0), target)
+            print(loss.item())
+
+            loss.backward()
+            optimizer.step()
+
+        print(inner_product)
+
+    # python -m unittest models.wrapped_sentence_transformer_test.TestOverfit.test_overfit_amp -v
+    def test_overfit_amp(self):
+        config = {
+            "architecture": {
+                "semantic_search_model": {
+                    "checkpoint": "all-mpnet-base-v2",
+                    "device": "cuda:0",
+                }
+            }
+        }
+        wrapped_model = WrappedSentenceTransformerModel(config)
+        query = "What color is the fruit that Alice loves?"
+        documents = [
+            "What fruit does Alice love?",
+            "What fruit does Bob love?",
+            "What is the color of apple?",
+            "How heavy is an apple?",
+        ]
+        wrapped_model.model.train()
+        target = torch.tensor([[1.0, 0.0, 1.0, 0.0]], device="cuda:0")
+
+        optimizer = optim.AdamW(wrapped_model.model.parameters(), lr=1e-6)
+        loss_fn = nn.MSELoss()
+        scaler = GradScaler()
+
+        for _ in range(100):
+            optimizer.zero_grad()
+
+            with autocast(dtype=torch.float16):
+                inner_product = wrapped_model.get_inner_products(query, documents)
+                loss = loss_fn(inner_product.unsqueeze(0), target)
+                print(loss.item())
+
+            # Optimizer step
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+        print(inner_product)
