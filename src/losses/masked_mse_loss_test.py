7b3e49696f751ff0e32362e99c2b7c4be4ad9c68
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2023-12-10 18:34:08 -0600

    test for batch

--- src/losses/masked_mse_loss_test.py
+++ src/losses/masked_mse_loss_test.py
@@ -7,11 +7,13 @@ from losses.masked_mse_loss import MaskedMSELoss
 class TestMaskedMSELoss(unittest.TestCase):
     # python -m unittest losses.masked_mse_loss_test.TestMaskedMSELoss.test_loss_computation -v
     def test_loss_computation(self):
-        input_tensor = torch.tensor([1.0, 2.0, 3.0], requires_grad=True)
-        target_tensor = torch.tensor([1.1, 2.2, 3.3])
-        mask_tensor = torch.tensor([False, True, False])
+        input_tensor = torch.tensor(
+            [[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]], requires_grad=True
+        )
+        target_tensor = torch.tensor([[1.1, 2.2, 3.3], [1.1, 2.2, 3.3]])
+        mask_tensor = torch.tensor([[False, True, False], [False, False, True]])
 
-        expected_loss = torch.tensor([0.049999989569187164])
+        expected_loss = torch.tensor([0.03750000149011612])
         loss_fn = MaskedMSELoss()
 
         loss = loss_fn(input_tensor, target_tensor, mask_tensor)
