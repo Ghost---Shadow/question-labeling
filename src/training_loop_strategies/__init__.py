2bd1366cf85cfd41109273bbffae477ec56165dd
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2023-12-11 09:31:58 -0600

    mixed precision

--- src/training_loop_strategies/__init__.py
+++ src/training_loop_strategies/__init__.py
@@ -1,3 +1,6 @@
 from training_loop_strategies import iterative_strategy
 
-TRAINING_LOOP_STRATEGY_LUT = {"iterative_strategy": iterative_strategy.train_step}
+TRAINING_LOOP_STRATEGY_LUT = {
+    "iterative_strategy": iterative_strategy.train_step,
+    "iterative_strategy_full_precision": iterative_strategy.train_step_full_precision,
+}
--- src/training_loop_strategies/iterative_strategy.py
+++ src/training_loop_strategies/iterative_strategy.py
@@ -1,7 +1,69 @@
 import torch
+from torch.cuda.amp import autocast, GradScaler
 
 
 def train_step(wrapped_model, optimizer, batch, aggregation_fn, loss_fn):
+    scaler = GradScaler()  # Initialize GradScaler
+
+    optimizer.zero_grad()
+
+    total_loss = 0.0
+    total_gradient_accumulation_steps = 0
+    for relevant_sentence_indexes in batch["relevant_sentence_indexes"]:
+        for _ in relevant_sentence_indexes:
+            total_gradient_accumulation_steps += 1
+
+    for question, flat_questions, selection_vector, relevant_sentence_indexes in zip(
+        batch["questions"],
+        batch["flat_questions"],
+        batch["selection_vector"],
+        batch["relevant_sentence_indexes"],
+    ):
+        picked_so_far = torch.zeros((len(flat_questions)), dtype=torch.bool)
+
+        labels = torch.tensor(
+            selection_vector, device=wrapped_model.device, dtype=torch.float32
+        )
+
+        for next_pick_index in relevant_sentence_indexes:
+            with autocast(dtype=torch.float16):
+                (
+                    question_embedding,
+                    document_embeddings,
+                ) = wrapped_model.get_query_and_document_embeddings(
+                    question, flat_questions
+                )
+
+                aggregated_query_embedding = aggregation_fn(
+                    question_embedding, document_embeddings, picked_so_far
+                )
+
+                # Already normalized
+                similarity = (
+                    aggregated_query_embedding @ document_embeddings.T
+                ).squeeze()
+
+                # Dont compute loss on items that are already picked
+                relevant_similarity = similarity[~picked_so_far]
+                relevant_labels = labels[~picked_so_far]
+
+                loss = loss_fn(relevant_similarity, relevant_labels)
+                loss = loss / total_gradient_accumulation_steps
+
+            scaler.scale(loss).backward()  # Scale the loss before backward
+
+            total_loss += loss.item()
+
+            # Pick one of the correct answers
+            picked_so_far[next_pick_index] = True
+
+    scaler.step(optimizer)
+    scaler.update()
+
+    return total_loss
+
+
+def train_step_full_precision(wrapped_model, optimizer, batch, aggregation_fn, loss_fn):
     optimizer.zero_grad()
 
     total_loss = 0.0
