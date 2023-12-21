0539567ea0fb69fe3021d299b9a6275a3a21ea75
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2023-12-21 10:29:31 -0600

    vectorize diversity gain

--- src/aggregation_strategies/submodular_mutual_information_strategy.py
+++ src/aggregation_strategies/submodular_mutual_information_strategy.py
@@ -108,6 +108,36 @@ class SubmodularMutualInformation(nn.Module):
 
         return diversity_gain_matrix
 
+    def _diversity_gain(self, question_embedding, filtered_document_embeddings):
+        # Aliases
+        q = question_embedding
+        d = filtered_document_embeddings
+        merge = self.merge
+
+        num_documents = d.shape[0]
+        diversity_gain_matrix = torch.zeros((num_documents, num_documents))
+
+        # Vectorized merge operation
+        q_d = merge(q.repeat(num_documents, 1), d)
+
+        # Compute cosine similarities in a vectorized way
+        # Reshape q_d to (num_documents, 1, embedding_size) and d to (1, num_documents, embedding_size)
+        # The resulting cosine_similarity matrix will be of shape (num_documents, num_documents)
+        cosine_sim_matrix = F.cosine_similarity(q_d.unsqueeze(1), d.unsqueeze(0), dim=2)
+
+        # Compute diversity for each pair
+        diversity_matrix = 1 - cosine_sim_matrix
+
+        # Sum over columns to get the diversity for each document after merging with the query
+        diversity_per_document = diversity_matrix.sum(dim=1)
+
+        # Compute the diversity gain matrix
+        diversity_gain_matrix = diversity_per_document.unsqueeze(
+            1
+        ) - diversity_per_document.unsqueeze(0)
+
+        return diversity_gain_matrix
+
     def forward(self, question_embedding, document_embeddings, mask):
         """
         Compute a weighted average embedding based on submodular mutual information scores.
