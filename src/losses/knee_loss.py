715e0838b3676f89c98b50217f605d0aaeaa6513
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2023-12-26 11:05:46 -0600

    Fix knee loss math

--- src/losses/knee_loss.py
+++ src/losses/knee_loss.py
@@ -2,24 +2,105 @@ import torch
 
 
 class KneeLoss(torch.nn.Module):
-    def forward(self, similarities, num_relevant):
-        # Set first derivative to be the similarities themselves
-        first_derivative = similarities
+    def __init__(self, config=None):
+        super(KneeLoss, self).__init__()
+        self.config = config
+        self.distractor_retardation_factor = 1e1
+        self.min_non_distractor_acceleration = 1e-2
 
-        # Compute second derivative
+    def _calculate_velocity_acceleration(self, displacement, time):
+        """
+        Calculate the initial velocity and constant acceleration to reach a given displacement in a given time.
+
+        displacement: The total displacement to be achieved.
+        time: The total time (number of steps) to achieve the displacement.
+        """
+        # Assuming final velocity is zero (decelerating to a stop), use kinematic equation: D = V0 * T - 0.5 * A * T^2
+        # Solving for initial velocity (V0) and acceleration (A)
+        acceleration = -self.min_non_distractor_acceleration
+        initial_velocity = (displacement - 0.5 * acceleration * time**2) / time
+
+        return (
+            initial_velocity,
+            acceleration,
+            initial_velocity / self.distractor_retardation_factor,
+            acceleration / self.distractor_retardation_factor,
+        )
+
+    def _ideal_curve(self, similarities, num_relevant):
+        min_non_distractor_displacement = num_relevant
+        (
+            min_non_distractor_velocity,
+            min_non_distractor_acceleration,
+            min_distractor_velocity,
+            min_distractor_acceleration,
+        ) = self._calculate_velocity_acceleration(
+            min_non_distractor_displacement, num_relevant
+        )
+
+        num_distractor = len(similarities) - num_relevant
+
+        # Create arrays for velocities and accelerations
+        non_distractor_velocities = torch.full(
+            (num_relevant,), min_non_distractor_velocity
+        )
+        non_distractor_accelerations = torch.full(
+            (num_relevant,), min_non_distractor_acceleration
+        )
+        distractor_velocities = torch.full((num_distractor,), min_distractor_velocity)
+        distractor_accelerations = torch.full(
+            (num_distractor,), min_distractor_acceleration
+        )
+
+        # Integrate acceleration to get velocity
+        non_distractor_velocities += torch.cumsum(non_distractor_accelerations, dim=0)
+        distractor_velocities += torch.cumsum(distractor_accelerations, dim=0)
+
+        # Combine velocities for non-distractors and distractors
+        total_velocities = torch.cat((non_distractor_velocities, distractor_velocities))
+
+        ideal_curve = total_velocities
+
+        return ideal_curve
+
+    def forward(self, similarities, num_relevant):
+        min_non_distractor_displacement = num_relevant
+        (
+            min_non_distractor_velocity,
+            min_non_distractor_acceleration,
+            min_distractor_velocity,
+            min_distractor_acceleration,
+        ) = self._calculate_velocity_acceleration(
+            min_non_distractor_displacement, num_relevant
+        )
+
+        # Compute first and second derivatives
+        first_derivative = similarities
         second_derivative = torch.diff(
             similarities, append=torch.tensor([0.0], device=similarities.device)
         )
 
         # Loss for non-distractors
-        non_distractor_loss = torch.sum(
-            torch.relu(-first_derivative[:num_relevant])
-        ) + torch.sum(torch.relu(second_derivative[:num_relevant]))
+        non_distractor_velocity_loss = torch.sum(
+            torch.relu(-first_derivative[:num_relevant] + min_non_distractor_velocity)
+        )
+        non_distractor_acceleration_loss = torch.sum(
+            torch.relu(
+                second_derivative[:num_relevant] - min_non_distractor_acceleration
+            )
+        )
+        non_distractor_loss = (
+            non_distractor_velocity_loss + non_distractor_acceleration_loss
+        )
 
         # Loss for distractors
-        distractor_loss = torch.sum(
-            torch.square(first_derivative[num_relevant:])
-        ) + torch.sum(torch.square(second_derivative[num_relevant:]))
+        distractor_velocity_loss = torch.sum(
+            torch.relu(first_derivative[num_relevant:] - min_distractor_velocity)
+        )
+        distractor_acceleration_loss = torch.sum(
+            torch.relu(second_derivative[num_relevant:] - min_distractor_acceleration)
+        )
+        distractor_loss = distractor_velocity_loss + distractor_acceleration_loss
 
         # Total loss
         total_loss = non_distractor_loss + distractor_loss
--- src/losses/knee_loss_test.py
+++ src/losses/knee_loss_test.py
@@ -16,9 +16,25 @@ class TestKneeLoss(unittest.TestCase):
             num_relevant + num_distractor, device="cpu", requires_grad=True
         )
 
-        loss_fn = KneeLoss()
+        loss_fn = KneeLoss({})
 
         loss = loss_fn(similarities, num_relevant)
+
+        # Should not crash
+        loss.backward()
+
+    # python -m unittest losses.knee_loss_test.TestKneeLoss.test_minima -v
+    def test_minima(self):
+        num_relevant = 2
+
+        similarities = torch.tensor(
+            [0, 0, 0, 0], requires_grad=True, dtype=torch.float32
+        )
+
+        loss_fn = KneeLoss({})
+
+        loss = loss_fn(similarities, num_relevant)
+        print(loss)
 
         # Should not crash
         loss.backward()
@@ -32,15 +48,15 @@ class TestKneeLoss(unittest.TestCase):
             num_relevant + num_distractor, device="cpu", requires_grad=True
         )
 
-        loss_fn = KneeLoss()
+        loss_fn = KneeLoss({})
 
-        optimizer = torch.optim.AdamW([similarities], lr=1e-2)
+        optimizer = torch.optim.AdamW([similarities], lr=1e-1)
 
         # Store the cumulative sum progress
         cumsum_progress = []
 
         num_epochs = 100
-        plot_every = 10
+        plot_every = 20
         for epoch in tqdm(range(num_epochs)):
             optimizer.zero_grad()
             loss = loss_fn(similarities, num_relevant)
@@ -49,13 +65,20 @@ class TestKneeLoss(unittest.TestCase):
             similarities.data = torch.clamp(similarities.data, 0, 1)
             if epoch % plot_every == 0 or epoch == num_epochs - 1:
                 cumsum_progress.append(
-                    similarities.data.clone().detach().cumsum(0).numpy()
+                    similarities.data.clone().detach().cumsum(0).cpu().numpy()
                 )
 
         # Plotting the cumulative sum evolution
         plt.figure(figsize=(12, 8))
         for i, cumsum in enumerate(cumsum_progress):
+            # if i < 1:
+            #     continue
             plt.plot(cumsum, marker="o", label=f"Epoch {i * plot_every}")
+        ideal_curve = loss_fn._ideal_curve(similarities, num_relevant).cumsum(0).numpy()
+        plt.plot(ideal_curve, color="r", linestyle="--", label=f"Ideal")
+        plt.axvline(
+            x=num_relevant, color="r", linestyle="--", label="Knee (x = num_relevant)"
+        )
         plt.title("Evolution of Cumulative Sum of Similarities Over Training (AdamW)")
         plt.xlabel("Document Index")
         plt.ylabel("Cumulative Sum of Similarities")
