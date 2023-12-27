a9bd9493a1ecbdc9d36154d083df7c5334dc0c2f
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2023-12-27 07:26:41 -0600

    update sanity check

--- src/dataset_generation_scripts/sanity_check_hotpot_qa_with_q.py
+++ src/dataset_generation_scripts/sanity_check_hotpot_qa_with_q.py
@@ -3,17 +3,23 @@ from datasets import load_dataset
 from tqdm import tqdm
 
 
-def check_ids_in_jsonl(file_path):
-    dataset = load_dataset("hotpot_qa", "distractor", split="validation")
+def check_ids_in_jsonl(file_path, split):
+    dataset = load_dataset("hotpot_qa", "distractor", split=split)
     seen_rows = 0
+    expected_length = min(10000, len(dataset))
     with open(file_path, "r") as f:
-        for line, actual_row in tqdm(zip(f, dataset), total=7405):
+        for line, actual_row in tqdm(zip(f, dataset), total=expected_length):
             row = json.loads(line)
             assert actual_row["id"] == row["id"]
             seen_rows += 1
 
-    assert seen_rows == len(dataset), seen_rows
+    assert seen_rows == expected_length, seen_rows
 
 
-file_path = "data/validation.jsonl"
-check_ids_in_jsonl(file_path)
+file_path = "data/hotpotqa_with_qa_gpt35/validation.jsonl"
+split = "validation"
+check_ids_in_jsonl(file_path, split)
+
+file_path = "data/hotpotqa_with_qa_gpt35/train.jsonl"
+split = "train"
+check_ids_in_jsonl(file_path, split)
