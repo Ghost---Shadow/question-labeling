9db99ed1902a30f3162645b2bbcb269711989905
Souradeep Nanda <souradeep.nanda123@gmail.com>
2023-12-30 23:37:06 +0000

    add stop instance command

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -1,17 +1,20 @@
-python ./src/train.py --config=experiments/gpt35_mpnet_triplet_only_d.yaml --debug
+# python ./src/train.py --config=experiments/gpt35_mpnet_triplet_only_d.yaml --debug
 python ./src/train.py --config=experiments/gpt35_mpnet_triplet_only_d.yaml
 
-python ./src/train.py --config=experiments/gpt35_mpnet_triplet_only_q.yaml --debug
+# python ./src/train.py --config=experiments/gpt35_mpnet_triplet_only_q.yaml --debug
 python ./src/train.py --config=experiments/gpt35_mpnet_triplet_only_q.yaml
 
-python ./src/train.py --config=experiments/gpt35_mpnet_ni_triplet.yaml --debug
+# python ./src/train.py --config=experiments/gpt35_mpnet_ni_triplet.yaml --debug
 python ./src/train.py --config=experiments/gpt35_mpnet_ni_triplet.yaml
 
-python ./src/train.py --config=experiments/gpt35_mpnet_triplet.yaml --debug
+# python ./src/train.py --config=experiments/gpt35_mpnet_triplet.yaml --debug
 python ./src/train.py --config=experiments/gpt35_mpnet_triplet.yaml
 
-python ./src/train.py --config=experiments/gpt35_mpnet_kldiv.yaml --debug
+# python ./src/train.py --config=experiments/gpt35_mpnet_kldiv.yaml --debug
 python ./src/train.py --config=experiments/gpt35_mpnet_kldiv.yaml
 
-python ./src/train.py --config=experiments/gpt35_mpnet_mse.yaml --debug
+# python ./src/train.py --config=experiments/gpt35_mpnet_mse.yaml --debug
 python ./src/train.py --config=experiments/gpt35_mpnet_mse.yaml
+
+gcloud compute instances stop q-labeling-1 --zone=us-central1-a --project=angular-unison-350808
+# python devops/stop_instance.py
