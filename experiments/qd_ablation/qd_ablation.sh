aad16ace0291bc6d8b3fe0b897ddf4f3f061aadc
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2024-01-09 17:00:54 -0600

    Chage QD to KL div

--- experiments/qd_ablation/qd_ablation.sh
+++ experiments/qd_ablation/qd_ablation.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 echo $HOSTNAME
 
-# python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_triplet_only_d.yaml --debug
-python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_triplet_only_d.yaml
+# python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_kldiv_only_d.yaml --debug
+python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_kldiv_only_d.yaml
 
-# python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_triplet_only_q.yaml --debug
-python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_triplet_only_q.yaml
+# python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_kldiv_only_q.yaml --debug
+python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_kldiv_only_q.yaml
 
 source devops/stop_current_gcp_instance.sh
