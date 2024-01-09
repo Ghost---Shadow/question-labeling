aa238f28041b939e439405d1e0dd64618cb165d9
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2024-01-08 20:21:01 -0600

    Loss fn sweep

---
+++ experiments/loss_fn_sweep/loss_fn_sweep_experiments.sh
@@ -0,0 +1,10 @@
+# python ./src/train.py --config=experiments/loss_fn_sweep/gpt35_mpnet_kldiv.yaml --debug
+python ./src/train.py --config=experiments/loss_fn_sweep/gpt35_mpnet_kldiv.yaml
+
+# python ./src/train.py --config=experiments/loss_fn_sweep/gpt35_mpnet_mse.yaml --debug
+python ./src/train.py --config=experiments/loss_fn_sweep/gpt35_mpnet_mse.yaml
+
+# python ./src/train.py --config=experiments/loss_fn_sweep/gpt35_mpnet_triplet.yaml --debug
+python ./src/train.py --config=experiments/loss_fn_sweep/gpt35_mpnet_triplet.yaml
+
+gcloud compute instances stop q-labeling-1 --zone=us-central1-a --project=angular-unison-350808
