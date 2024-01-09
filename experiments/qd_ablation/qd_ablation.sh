ef30aaed127764e8a2a3676d9bdd1d209da98426
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2024-01-09 17:21:17 -0600

    Add QD Experiment

--- experiments/qd_ablation/qd_ablation.sh
+++ experiments/qd_ablation/qd_ablation.sh
@@ -9,4 +9,7 @@ python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_kldiv_only_d.
 # python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_kldiv_only_q.yaml --debug
 python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_kldiv_only_q.yaml
 
+# python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_kldiv_qd.yaml --debug
+python ./src/train.py --config=experiments/qd_ablation/gpt35_mpnet_kldiv_qd.yaml
+
 source devops/stop_current_gcp_instance.sh
