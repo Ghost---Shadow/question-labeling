bca5ca1da80d546818d1669dccee901cb0948cf9
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2024-01-10 10:16:19 -0600

    Remove full precision option

--- src/training_loop_strategies/__init__.py
+++ src/training_loop_strategies/__init__.py
@@ -2,10 +2,6 @@ from training_loop_strategies import iterative_strategy, non_iterative_strategy
 
 TRAINING_LOOP_STRATEGY_LUT = {
     "iterative_strategy": (iterative_strategy.train_step, iterative_strategy.eval_step),
-    "iterative_strategy_full_precision": (
-        iterative_strategy.train_step_full_precision,
-        iterative_strategy.eval_step,
-    ),
     "non_iterative_strategy": (
         non_iterative_strategy.train_step,
         non_iterative_strategy.eval_step,
