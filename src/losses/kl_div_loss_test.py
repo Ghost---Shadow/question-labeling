Fix KL div test

src/losses/kl_div_loss_test.py
@@ -7,16 +7,14 @@ from losses.kl_div_loss import KLDivLoss
 class TestKLDivLoss(unittest.TestCase):
     # python -m unittest losses.kl_div_loss_test.TestKLDivLoss.test_loss_computation -v
     def test_loss_computation(self):
-        input_tensor = torch.tensor(
-            [[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]], requires_grad=True
-        )
-        target_tensor = torch.tensor([[1.1, 2.2, 3.3], [1.1, 2.2, 3.3]])
+        input_tensor = torch.tensor([0.9, 0.7, 0.6], requires_grad=True)
+        target_tensor = torch.tensor([1.0, 0.0, 1.0])
 
         # Convert to probability distributions
         input_prob = F.softmax(input_tensor, dim=-1)
         target_prob = F.softmax(target_tensor, dim=-1)
 
-        expected_loss = torch.tensor([0.00021061301231384277])
+        expected_loss = torch.tensor([0.0023906927090138197])
 
         loss_fn = KLDivLoss()
 