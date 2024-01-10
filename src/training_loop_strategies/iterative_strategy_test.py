f9acc8d6a49e9af10a855bb8b11d9971d7c335c2
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2024-01-10 09:39:06 -0600

    Remove full precision trainstep

--- src/training_loop_strategies/iterative_strategy_test.py
+++ src/training_loop_strategies/iterative_strategy_test.py
@@ -73,37 +73,5 @@ class TestEvalStep(unittest.TestCase):
         print(metrics)
 
 
-# python -m unittest training_loop_strategies.iterative_strategy_test.TestTrainStep -v
-# @unittest.skip("needs gpu")
-class TestTrainStep(unittest.TestCase):
-    # python -m unittest training_loop_strategies.iterative_strategy_test.TestTrainStep.test_full_precision -v
-    def test_full_precision(self):
-        config = {
-            "architecture": {
-                "semantic_search_model": {
-                    "checkpoint": "sentence-transformers/all-mpnet-base-v2",
-                    "device": "cuda:0",
-                },
-            },
-            "eval": {"k": [1, 5, 10]},
-        }
-        wrapped_model = WrappedMpnetModel(config)
-        optimizer = optim.AdamW(wrapped_model.model.parameters(), lr=1e-5)
-
-        batch_size = 2
-
-        train_loader = get_train_loader(batch_size)
-
-        batch = next(iter(train_loader))
-        loss_fn = TripletLoss(config)
-        scaler = None
-
-        for _ in range(10):
-            metrics = train_step_full_precision(
-                config, scaler, wrapped_model, optimizer, batch, loss_fn
-            )
-            print(metrics)
-
-
 if __name__ == "__main__":
     unittest.main()
