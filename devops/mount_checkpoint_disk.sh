9c97d1fdd67a8a0ea493f5f1d8dc9c596df04286
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-01-11 14:39:46 +0000

    Artifacts directory

--- devops/mount_checkpoint_disk.sh
+++ devops/mount_checkpoint_disk.sh
@@ -2,8 +2,16 @@ lsblk
 sudo mkdir /mnt/checkpoints
 # sudo mkfs.ext4 /dev/nvme0n2
 sudo mount /dev/nvme0n2 /mnt/checkpoints
+# rm ./checkpoints
+
+# ./checkpoints directory
 sudo mkdir /mnt/checkpoints/checkpoints
-rm ./checkpoints
 ln -s /mnt/checkpoints/checkpoints ./checkpoints
 sudo chown -R $(whoami):$(whoami) /mnt/checkpoints/checkpoints
 sudo chmod 766 /mnt/checkpoints/checkpoints
+
+# ./artifacts directory
+sudo mkdir /mnt/checkpoints/artifacts
+ln -s /mnt/checkpoints/artifacts ./artifacts
+sudo chown -R $(whoami):$(whoami) /mnt/checkpoints/artifacts
+sudo chmod 766 /mnt/checkpoints/artifacts
