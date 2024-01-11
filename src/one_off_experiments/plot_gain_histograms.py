2e9945f1211b16505797685b73ece4ce21c993d6
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2024-01-11 10:48:47 -0600

    MErge plots

--- src/one_off_experiments/plot_gain_histograms.py
+++ src/one_off_experiments/plot_gain_histograms.py
@@ -1,68 +1,96 @@
-import argparse
 import json
+from pathlib import Path
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+from tqdm import tqdm
 
-# python src/one_off_experiments/plot_gain_histograms.py --json_file=artifacts/checkpoint_evals/mpnet_hotpot_qa_with_q_a6b5a.json
-
-# Define and parse command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("--json_file", type=str, help="Path to the JSON file")
-args = parser.parse_args()
-
-# Load data from JSON file
-with open(args.json_file, "r") as file:
-    data = json.load(file)
-
-experiment_name = data["config"]["architecture"]["semantic_search_model"][
-    "real_checkpoint"
-]
-experiment_name = experiment_name.split("/")
-experiment_name = experiment_name[min(2, len(experiment_name) - 1)]
-
-# Ensure the output directory exists
-output_dir = f"./artifacts/gain_histogram/{experiment_name}"
-os.makedirs(output_dir, exist_ok=True)
-
-# Extract data from 'gain_cutoff_histogram' key
-data_dict = data["gain_cutoff_histogram"]
-gain_histogram_resolution = data["gain_histogram_resolution"]
-
-rows = []
-for gain, metrics_list in data_dict.items():
-    for metrics in metrics_list:
-        row = {"gain": gain}
-        row.update(metrics)
-        rows.append(row)
-
-df = pd.DataFrame(rows)
-df["gain"] = pd.to_numeric(df["gain"])
-df["gain"] = df["gain"] * gain_histogram_resolution
-
-# Step 3: Create and save plots for each metric
-metric_names = ["recall_at_6969", "precision_at_6969", "f1_at_6969", "real_k"]
-name_map = {
+NAME_MAP = {
     "recall_at_6969": "recall",
     "precision_at_6969": "precision",
     "f1_at_6969": "f1",
     "real_k": "k",
 }
-for metric in metric_names:
-    # metric_df = df.groupby("gain")[metric].agg(["mean", "std"]).reset_index()
-    # metric_df["std"] = metric_df["std"].fillna(0)
 
-    plt.figure()
+EXPERIMENT_NAME_MAP = {
+    "gpt35_mpnet_kldiv_qd": "quality_diversity",
+    "gpt35_mpnet_kldiv_only_q": "only_q",
+    "gpt35_mpnet_kldiv_only_d": "only_d",
+    "gpt35_mpnet_ni_kldiv": "non_iterative",
+}
 
-    # sns.barplot(metric_df, x="gain", y="mean", yerr=metric_df["std"])
-    # sns.barplot(df, x="gain", y=metric)
-    sns.lineplot(df, x="gain", y=metric)
-    metric_name = name_map[metric]
-    plt.title(f"{experiment_name}: {metric_name} by gain")
-    plt.xlabel("Gain")
-    plt.ylabel(metric_name)
 
-    # Save the plot
-    plt.savefig(f"{output_dir}/{metric_name}_plot.png")
-    plt.close()
+def format_experiment_name(raw_name):
+    for candidate_name, formatted_name in EXPERIMENT_NAME_MAP.items():
+        if raw_name.startswith(candidate_name):
+            return formatted_name
+
+    return raw_name
+
+
+def json_dir_to_df(base_path):
+    base_path = Path(base_path)
+    rows = []
+
+    for file_name in tqdm(os.listdir(base_path)):
+        if not file_name.endswith(".json"):
+            continue
+
+        file_path = base_path / file_name
+        with open(file_path, "r") as file:
+            data = json.load(file)
+
+        experiment_name = data["config"]["architecture"]["semantic_search_model"][
+            "real_checkpoint"
+        ]
+        experiment_name = experiment_name.split("/")
+        experiment_name = experiment_name[min(2, len(experiment_name) - 1)]
+
+        # Extract data from 'gain_cutoff_histogram' key
+        data_dict = data["gain_cutoff_histogram"]
+        gain_histogram_resolution = data["gain_histogram_resolution"]
+
+        for gain, metrics_list in data_dict.items():
+            for metrics in metrics_list:
+                row = {
+                    **metrics,
+                    "gain": int(gain) * gain_histogram_resolution,
+                    "experiment": format_experiment_name(experiment_name),
+                }
+                row.update(metrics)
+                rows.append(row)
+
+    df = pd.DataFrame(rows)
+
+    df.rename(columns=NAME_MAP, inplace=True)
+
+    return df
+
+
+def plot_df(df):
+    # Ensure the output directory exists
+    output_dir = Path("./artifacts/gain_histogram")
+    output_dir.mkdir(exist_ok=True, parents=True)
+
+    metric_names = list(NAME_MAP.values())
+    for metric in tqdm(metric_names):
+        plt.figure()
+
+        sns.lineplot(df, x="gain", y=metric, hue="experiment")
+        plt.title(f"{metric} by gain")
+        plt.xlabel("Gain")
+        plt.ylabel(metric)
+        plt.ylim(0, 1)
+
+        # Save the plot
+        plt.savefig(output_dir / f"{metric}_plot.png")
+        plt.close()
+
+
+if __name__ == "__main__":
+    df = json_dir_to_df("artifacts/checkpoint_evals")
+    df = df[df["gain"] <= 0.5]
+    plot_df(df)
+    # max_df = df.groupby("experiment").max()
+    # print(max_df)
