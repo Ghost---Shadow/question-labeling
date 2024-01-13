6d50ea6f802eb6b100bfdac8554e66f0fd7e07e5
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2024-01-13 12:19:51 -0600

    Plot gain curves

--- src/one_off_experiments/plot_gain_histograms.py
+++ src/one_off_experiments/plot_gain_histograms.py
@@ -1,17 +1,13 @@
 import json
 from pathlib import Path
 import pandas as pd
+from pydash import get
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 from tqdm import tqdm
 
-NAME_MAP = {
-    "recall_at_6969": "recall",
-    "precision_at_6969": "precision",
-    "f1_at_6969": "f1",
-    "real_k": "k",
-}
+METRICS = ["recall", "precision", "f1", "real_k"]
 
 EXPERIMENT_NAME_MAP = {
     "gpt35_mpnet_kldiv_qd": "quality_diversity",
@@ -22,9 +18,8 @@ EXPERIMENT_NAME_MAP = {
 
 
 def format_experiment_name(raw_name):
-    for candidate_name, formatted_name in EXPERIMENT_NAME_MAP.items():
-        if raw_name.startswith(candidate_name):
-            return formatted_name
+    if raw_name in EXPERIMENT_NAME_MAP:
+        return EXPERIMENT_NAME_MAP[raw_name]
 
     return raw_name
 
@@ -41,11 +36,7 @@ def json_dir_to_df(base_path):
         with open(file_path, "r") as file:
             data = json.load(file)
 
-        experiment_name = data["config"]["architecture"]["semantic_search_model"][
-            "real_checkpoint"
-        ]
-        experiment_name = experiment_name.split("/")
-        experiment_name = experiment_name[min(2, len(experiment_name) - 1)]
+        experiment_name = get(data, "config.wandb.name", "baseline")
 
         # Extract data from 'gain_cutoff_histogram' key
         data_dict = data["gain_cutoff_histogram"]
@@ -63,8 +54,6 @@ def json_dir_to_df(base_path):
 
     df = pd.DataFrame(rows)
 
-    df.rename(columns=NAME_MAP, inplace=True)
-
     return df
 
 
@@ -73,15 +62,16 @@ def plot_df(df):
     output_dir = Path("./artifacts/gain_histogram")
     output_dir.mkdir(exist_ok=True, parents=True)
 
-    metric_names = list(NAME_MAP.values())
-    for metric in tqdm(metric_names):
+    # for metric in tqdm(METRICS):
+    for metric in tqdm(["f1", "real_k"]):
         plt.figure()
 
         sns.lineplot(df, x="gain", y=metric, hue="experiment")
         plt.title(f"{metric} by gain")
         plt.xlabel("Gain")
         plt.ylabel(metric)
-        plt.ylim(0, 1)
+        if metric != "real_k":
+            plt.ylim(0, 1)
 
         # Save the plot
         plt.savefig(output_dir / f"{metric}_plot.png")
@@ -90,7 +80,8 @@ def plot_df(df):
 
 if __name__ == "__main__":
     df = json_dir_to_df("artifacts/checkpoint_evals")
-    df = df[df["gain"] <= 0.5]
+    df = df[df["gain"] <= 0.6]
+    # df.to_csv("./artifacts/gain_curves.csv")
     max_df = df.groupby(["gain", "experiment"]).mean().reset_index()
     max_df = max_df.groupby(["experiment"]).max()
     print(max_df)
