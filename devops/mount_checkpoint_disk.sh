4ff3a5557463448f1a246828a62bd553474c7c3c
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-01-13 00:45:23 +0000

    Mount chekcpoint disk for nvme and regular

--- devops/mount_checkpoint_disk.sh
+++ devops/mount_checkpoint_disk.sh
@@ -1,17 +1,17 @@
-lsblk
-sudo mkdir /mnt/checkpoints
+# lsblk
+# sudo mkdir /home/jupyter
 # sudo mkfs.ext4 /dev/nvme0n2
-sudo mount /dev/nvme0n2 /mnt/checkpoints
+# sudo mount /dev/nvme0n2 /home/jupyter
 # rm ./checkpoints
 
 # ./checkpoints directory
-sudo mkdir /mnt/checkpoints/checkpoints
-ln -s /mnt/checkpoints/checkpoints ./checkpoints
-sudo chown -R $(whoami):$(whoami) /mnt/checkpoints/checkpoints
-sudo chmod 766 /mnt/checkpoints/checkpoints
+sudo mkdir /home/jupyter/checkpoints
+ln -s /home/jupyter/checkpoints ./checkpoints
+sudo chown -R $(whoami):$(whoami) /home/jupyter/checkpoints
+sudo chmod 766 /home/jupyter/checkpoints
 
 # ./artifacts directory
-sudo mkdir /mnt/checkpoints/artifacts
-ln -s /mnt/checkpoints/artifacts ./artifacts
-sudo chown -R $(whoami):$(whoami) /mnt/checkpoints/artifacts
-sudo chmod 766 /mnt/checkpoints/artifacts
+sudo mkdir /home/jupyter/artifacts
+ln -s /home/jupyter/artifacts ./artifacts
+sudo chown -R $(whoami):$(whoami) /home/jupyter/artifacts
+sudo chmod 766 /home/jupyter/artifacts
