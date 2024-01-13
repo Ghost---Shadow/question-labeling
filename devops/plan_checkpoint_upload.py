c4baa92255c25d76a6231aab1b02a5e9688a3508
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2024-01-13 09:37:31 -0600

    Refactor checkpoint upload

--- devops/plan_checkpoint_upload.py
+++ devops/plan_checkpoint_upload.py
@@ -1,23 +1,39 @@
 import os
 import glob
 
-base_path = "./checkpoints/"
-bucket_name = "q-labeling"
-shell_script_file = "./upload_checkpoints.sh"
 
-with open(shell_script_file, "w") as script:
-    script.write("#!/bin/bash\n\n")
-    for experiment_dir in glob.glob(f"{base_path}*/"):
-        experiment_name = os.path.basename(os.path.dirname(experiment_dir))
-        for seed_dir in glob.glob(f"{experiment_dir}seed_*/"):
-            seed = os.path.basename(os.path.dirname(seed_dir))
-            checkpoint_pattern = f"{seed_dir}epoch_*.pth"
-            list_of_files = glob.glob(checkpoint_pattern)
-            if list_of_files:
-                latest_checkpoint = max(list_of_files, key=os.path.getctime)
-                destination_blob_name = f"{bucket_name}/checkpoints/{experiment_name}/{seed}/{os.path.basename(latest_checkpoint)}"
+def find_latest_checkpoint(experiment_dir):
+    """
+    Finds the latest checkpoint for a given experiment and seed.
+    """
+    latest_checkpoints = []
+    for seed_dir in glob.glob(f"{experiment_dir}seed_*/"):
+        checkpoint_pattern = f"{seed_dir}epoch_*.pth"
+        list_of_files = glob.glob(checkpoint_pattern)
+        if list_of_files:
+            latest_checkpoint = max(list_of_files, key=os.path.getctime)
+            latest_checkpoints.append(latest_checkpoint)
+    return latest_checkpoints
+
+
+if __name__ == "__main__":
+    # Base path for checkpoints
+    base_path = "./checkpoints/"
+    bucket_name = "q-labeling"
+    shell_script_file = "./devops/upload_checkpoints.sh"
+
+    with open(shell_script_file, "w") as script:
+        script.write("#!/bin/bash\n\n")
+        for experiment_dir in glob.glob(f"{base_path}*/"):
+            experiment_name = os.path.basename(os.path.dirname(experiment_dir))
+
+            # Find the latest checkpoint for each seed in the experiment
+            latest_checkpoints = find_latest_checkpoint(experiment_dir)
+
+            for checkpoint in latest_checkpoints:
+                destination_blob_name = f"{bucket_name}/checkpoints/{experiment_name}/{os.path.basename(os.path.dirname(checkpoint))}/{os.path.basename(checkpoint)}"
                 script.write(
-                    f'gsutil cp "{latest_checkpoint}" "gs://{destination_blob_name}"\n'
+                    f'gsutil cp "{checkpoint}" "gs://{destination_blob_name}"\n'
                 )
 
-print(f"Written to {shell_script_file}")
+    print(f"Written to {shell_script_file}")
