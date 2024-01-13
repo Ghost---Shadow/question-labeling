e1ece5ca998a260a8a892ff842bde2df5ed07cb3
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2024-01-13 09:15:31 -0600

    Backup checkpoints script

--- devops/backup_all_checkpoints.sh
+++ devops/backup_all_checkpoints.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Source directory
-SOURCE_DIR=/mnt/checkpoints/checkpoints
+python devops/find_most_recent_checkpoints.py
 
-# Google Cloud Storage bucket name
-GCS_BUCKET=q-labeling
+source upload_checkpoints.sh
 
-# Recursively upload the files to the bucket
-gsutil -m cp -r $SOURCE_DIR/* gs://$GCS_BUCKET/
-
-# Notification
-echo "Upload completed. Files from $SOURCE_DIR uploaded to gs://$GCS_BUCKET/"
+source devops/stop_current_gcp_instance.sh
---
+++ devops/find_most_recent_checkpoints.py
@@ -0,0 +1,23 @@
+import os
+import glob
+
+base_path = "./checkpoints/"
+bucket_name = "q-labeling"
+shell_script_file = "./upload_checkpoints.sh"
+
+with open(shell_script_file, "w") as script:
+    script.write("#!/bin/bash\n\n")
+    for experiment_dir in glob.glob(f"{base_path}*/"):
+        experiment_name = os.path.basename(os.path.dirname(experiment_dir))
+        for seed_dir in glob.glob(f"{experiment_dir}seed_*/"):
+            seed = os.path.basename(os.path.dirname(seed_dir))
+            checkpoint_pattern = f"{seed_dir}epoch_*.pth"
+            list_of_files = glob.glob(checkpoint_pattern)
+            if list_of_files:
+                latest_checkpoint = max(list_of_files, key=os.path.getctime)
+                destination_blob_name = f"{bucket_name}/{experiment_name}/{seed}/{os.path.basename(latest_checkpoint)}"
+                script.write(
+                    f'gsutil cp "{latest_checkpoint}" "gs://checkpoints/{destination_blob_name}"\n'
+                )
+
+print(f"Written to {shell_script_file}")
