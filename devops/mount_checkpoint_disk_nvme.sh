f105036dc7480ddc7780500dc0aad7d05b3db4ab
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-01-13 01:50:30 +0000

    fstab mount script

--- devops/mount_checkpoint_disk_nvme.sh
+++ devops/mount_checkpoint_disk_nvme.sh
@@ -15,3 +15,12 @@ sudo mkdir /mnt/checkpoints/artifacts
 ln -s /mnt/checkpoints/artifacts ./artifacts
 sudo chown -R $(whoami):$(whoami) /mnt/checkpoints/artifacts
 sudo chmod 766 /mnt/checkpoints/artifacts
+
+# Send to fstab in case of reboot
+LINE='/dev/nvme0n2 /mnt/checkpoints ext4 defaults 0 2'
+
+if ! grep -Fxq "$LINE" /etc/fstab; then
+    echo "$LINE" | sudo tee -a /etc/fstab
+else
+    echo "$LINE already exists in /etc/fstab"
+fi
