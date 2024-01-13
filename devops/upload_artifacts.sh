443b7bd5f5e72e2a7531d6844074e592d1ee920b
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-01-13 23:37:03 +0000

    Dont zip it

--- devops/upload_artifacts.sh
+++ devops/upload_artifacts.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-# Directory to be zipped
-REMOTE_DIR=/mnt/checkpoints/artifacts
-ZIP_NAME=artifacts.zip
-
-# Google Cloud Storage bucket name
+DIR=./artifacts
 GCS_BUCKET=q-labeling
 
-# Zip the directory
-zip -r $ZIP_NAME $REMOTE_DIR
-
-# Upload the zip file to Google Cloud Storage bucket
-gsutil cp $ZIP_NAME gs://$GCS_BUCKET/
-
-# Remove the zip file from the remote server
-rm $ZIP_NAME
-
-# Notification
-echo "Upload completed. File $ZIP_NAME uploaded to gs://$GCS_BUCKET/"
+gsutil cp -r $DIR gs://$GCS_BUCKET/artifacts
