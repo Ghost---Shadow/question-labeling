5b973345e2168c000ac8ad5a1511654fe5cba069
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-01-19 14:43:40 +0000

    Add more experiments

--- experiments/cross_model_generalization/cross_model_generalization_experiments.sh
+++ experiments/cross_model_generalization/cross_model_generalization_experiments.sh
@@ -4,9 +4,12 @@
 echo $HOSTNAME
 
 # python ./src/train.py --config=experiments/cross_model_generalization/gpt35_beamr_kldiv.yaml --debug
-python ./src/train.py --config=experiments/cross_model_generalization/gpt35_beamr_kldiv.yaml
+# python ./src/train.py --config=experiments/cross_model_generalization/gpt35_beamr_kldiv.yaml
 
-# # python ./src/train.py --config=experiments/cross_model_generalization/gpt35_dbertav3_kldiv.yaml --debug
-# python ./src/train.py --config=experiments/cross_model_generalization/gpt35_dbertav3_kldiv.yaml
+# python ./src/train.py --config=experiments/cross_model_generalization/gpt35_deberta_kldiv_qd.yaml --debug
+python ./src/train.py --config=experiments/cross_model_generalization/gpt35_deberta_kldiv_qd.yaml
+
+# python ./src/train.py --config=experiments/cross_model_generalization/gpt35_deberta_ni_kldiv.yaml --debug
+python ./src/train.py --config=experiments/cross_model_generalization/gpt35_deberta_ni_kldiv.yaml
 
 source devops/stop_current_gcp_instance.sh
