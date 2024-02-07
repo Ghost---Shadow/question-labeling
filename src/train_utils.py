b80f80f9647329146e2b966bccaf4b45bd8dce10
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-02-07 09:02:56 -0600

    random and numpy seed

--- src/train_utils.py
+++ src/train_utils.py
@@ -7,6 +7,8 @@ import wandb
 
 
 def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
